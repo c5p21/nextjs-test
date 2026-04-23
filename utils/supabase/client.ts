00c107ef342fdb6f124de4424d1b9635b5a35714
李高阳 <1519103730@qq.com>
2026-04-23 18:16:11 +0800

    .env

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,8 +1,15 @@
-import { createBrowserClient  } from '@supabase/ssr'
+import { createBrowserClient } from '@supabase/ssr'
 
-// 从.env.local自动读取环境变量
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+export function createClient() {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
 
-// 导出supabase实例，全项目通用
-export const supabase = createBrowserClient (supabaseUrl, supabaseKey)
+  if (!supabaseUrl || !supabaseKey) {
+    console.error('❌ 环境变量缺失！')
+    return null
+  }
+
+  return createBrowserClient(supabaseUrl, supabaseKey)
+}
+
+export const supabase = createClient()!
